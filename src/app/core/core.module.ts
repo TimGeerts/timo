e2589443dbc5e7191f54cddaa0af0971791eca62
Tim Geerts <tim.geerts1@gmail.com>
2018-04-15 20:19:42 +0200

    removed angularfire2

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -4,25 +4,20 @@ import { DbService } from './db.service';
 import { HttpClientModule } from '@angular/common/http';
 import { AngularFireModule } from 'angularfire2';
 import { AngularFireDatabaseModule } from 'angularfire2/database';
-// import { AngularFireAuthModule } from 'angularfire2/auth';
 import { environment } from '../../environments/environment';
-
 
 @NgModule({
     imports: [
         HttpClientModule,
         AngularFireModule.initializeApp(environment.firebase),
         AngularFireDatabaseModule
-        // AngularFireAuthModule
     ],
     exports: [],
     declarations: [],
     providers: [
         TsmService, 
         DbService,
-        HttpClientModule, 
-        // AngularFirestoreModule,
-        // AngularFireDatabaseModule
+        HttpClientModule
     ],
 })
 export class CoreModule { }
